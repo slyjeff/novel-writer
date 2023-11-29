18c112e4528bf57ff830f0a356618868d34e62de
Jeff Sylvester <sylvesterjj@gmail.com>
2023-11-28 21:51:54 -0500

    Added bullet list control

---
+++ NovelDocs/Controls/BulletListControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+
+namespace NovelDocs.Controls; 
+
+public partial class BulletListControl {
+    public BulletListControl() {
+        InitializeComponent();
+    }
+
+    public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached("Text", typeof(string), typeof(BulletListControl), new UIPropertyMetadata(string.Empty, new PropertyChangedCallback(OnTextChange)));
+
+
+    public string Text {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    private static void OnTextChange(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        if (d is not BulletListControl bulletListControl) {
+            return;
+        }
+
+        var text = e.NewValue as string;
+        if (string.IsNullOrEmpty(text)) {
+            bulletListControl.ItemsControl.Visibility = Visibility.Hidden;
+            return;
+        }
+
+        bulletListControl.ItemsControl.ItemsSource = text.Split(Environment.NewLine);
+    }
+}
